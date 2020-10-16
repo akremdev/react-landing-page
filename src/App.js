daeb9f333e967a02443a566593b1fbf7c1c05e94
AkremDev <akremdev@pop-os.localdomain>
2020-10-16 23:04:58 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/services" component={Services}></Route>
+          <Route path="/products" component={Products}></Route>
+          <Route path="/sign-up" component={SignUp}></Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
